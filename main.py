d2b5c71e431021cb625d32ea93f983674cb20472
Nsai-roshan <81345700+Nsai-roshan@users.noreply.github.com>
2024-04-10 16:50:12 -0700

    local maxima
    implemented a function to find local maxima range from the spectrogram

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from connect import insert, fetch, update, delete_sound
 from config import SOUNDS
-from sound_recognition import create_spectrogram
+from sound_recognition import create_spectrogram, find_local_maxima, analyze_relationship
 import os
 
 # Demo usage
@@ -13,6 +13,8 @@ if __name__ == "__main__":
             print("3. Update an entry")
             print("4. Delete an entry")
             print("5. Show spectrogram")
+            print("6. Find and Show Local Maxima in Spectrogram")
+            print("7. Show Local Maxima analysis between two sound files")
             print("0. Exit")
 
             choice = input("Enter your choice: ")
@@ -44,6 +46,17 @@ if __name__ == "__main__":
                 print(os.listdir(SOUNDS))
                 file_name = input("Please choose a file: ")
                 create_spectrogram(file_name)
+            elif choice == "6":
+                print("Available sound files:", end=" ")
+                print(os.listdir(SOUNDS))
+                file_name = input("Please choose a file: ")
+                find_local_maxima(file_name)
+            # elif choice == "7":
+            #     print("Available sound files:", end=" ")
+            #     print(os.listdir(SOUNDS))
+            #     file1 = input("Please choose the first file: ")
+            #     file2 = input("Please choose the second file: ")
+            #     analyze_relationship(file1, file2)
             elif choice == "0":
                 break
             else:
